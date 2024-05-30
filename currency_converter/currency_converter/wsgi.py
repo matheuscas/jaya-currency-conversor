96fb2dd4314c338d5b162d3429d73ba2e15a8d18
Matheus Cardoso <matheus.mcas@gmail.com>
2024-05-30 13:31:11 -0300

    chore: add pre-commit and run initial config against all files

--- currency_converter/currency_converter/wsgi.py
+++ currency_converter/currency_converter/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currency_converter.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "currency_converter.settings")
 
 application = get_wsgi_application()
