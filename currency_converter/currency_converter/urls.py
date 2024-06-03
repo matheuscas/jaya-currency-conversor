f0dfbc19a2c8f124c5d315426f79c8dea9b3bdf4
Matheus Cardoso <matheus.mcas@gmail.com>
2024-06-02 22:06:43 -0300

    chore: make swager-ui the start page

--- currency_converter/currency_converter/urls.py
+++ currency_converter/currency_converter/urls.py
@@ -29,15 +29,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("conversion.urls")),
     # YOUR PATTERNS
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
     path(
-        "api/schema/swagger-ui/",
+        "",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
